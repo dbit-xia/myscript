9ebe441a8defbd3ced1ed12ce4f0fb290f4a0127
夏子省 <zisheng.xia@weimob.com>
2021-08-05 13:44:04 +0800

    add pgsql.sh

--- pgsql.sh
+++ pgsql.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
 
-docker run -e POSTGRESQL_PASSWORD=dbit bitnami/postgresql:latest
+#docker run --rm -e POSTGRESQL_PASSWORD=dbit -p 5432:5432 bitnami/postgresql:latest
+
+mkdir /data/pgsql -p
+chown 1001 /data/pgsql
+
+docker run --rm  --name pgsql \
+-e POSTGRESQL_USERNAME=dbit \
+-e POSTGRESQL_PASSWORD=dbit \
+-p 5432:5432 \
+-v /data/pgsql:/bitnami/postgresql \
+bitnami/postgresql:latest
