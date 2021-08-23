a730bc3eff4fbd8940b661a169f4877fb1672ff2
zisheng.xia <zisheng.xia@weimob.com>
2021-08-23 15:56:21 +0800

    普通用户docker

--- redis.sh
+++ redis.sh
@@ -2,11 +2,11 @@
 
 #docker run --rm -e POSTGRESQL_PASSWORD=dbit -p 5432:5432 bitnami/postgresql:latest
 
-mkdir /data/redis -p
-chown 1001 /data/redis
+mkdir ~/data/redis -p
+#chown 1001 ~/data/redis
 
 docker run --name redis \
     -p 6379:6379 \
     -e ALLOW_EMPTY_PASSWORD=yes \
-    -v /data/redis:/bitnami/redis/data \
+    -v /home/dbit/data/redis:/bitnami/redis/data \
     bitnami/redis:latest
