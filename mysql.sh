4a1b59f25e54d06b17eb78d3957506f768250f54
zisheng.xia <zisheng.xia@weimob.com>
2021-08-24 14:19:40 +0800

    普通用户docker

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 #docker stop mysql
 #docker rm mysql
-docker run -p 3306:3306 -p 33060:33060 --name mysql -v /etc/mysql/conf.d:/etc/mysql/conf.d -e MYSQL_ROOT_PASSWORD=runsa -d mysql
+docker run -p 3306:3306 -p 33060:33060 --name mysql -v /etc/mysql/conf.d:/etc/mysql/conf.d -e MYSQL_ROOT_PASSWORD=123456 -d mysql
+
+skip-grant-tables
+flush privileges
+CREATE USER 'dbit'@'%' IDENTIFIED with mysql_native_password  BY 'dbit'
+grant all on *.* to dbit@'localhost'
 
  
+#!/bin/sh
+
+#docker run --rm -e POSTGRESQL_PASSWORD=dbit -p 5432:5432 bitnami/postgresql:latest
+
+mkdir ~/data/mysql -p
+#chown 1001 /data/pgsql
+
+docker run --name mysql \
+-e ALLOW_EMPTY_PASSWORD=yes \
+-p 3306:3306 \
+-v /home/dbit/data/mysql:/bitnami/mysql/data \
+bitnami/mysql:latest
+
+
+docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password123 bitnami/mysql:latest
--- pgsql.sh
+++ pgsql.sh
@@ -2,12 +2,12 @@
 
 #docker run --rm -e POSTGRESQL_PASSWORD=dbit -p 5432:5432 bitnami/postgresql:latest
 
-mkdir /data/pgsql -p
-chown 1001 /data/pgsql
+mkdir ~/data/pgsql -p
+#chown 1001 /data/pgsql
 
 docker run --name pgsql \
 -e POSTGRESQL_USERNAME=dbit \
 -e POSTGRESQL_PASSWORD=dbit \
 -p 5432:5432 \
--v /data/pgsql:/bitnami/postgresql \
+-v /home/dbit/data/pgsql:/bitnami/postgresql \
 bitnami/postgresql:latest
